5cede39b24fe8be1666004238389375108a39355
Ivan Mikhalchenko <ivan.mikhalchenko@genesys.com>
2022-07-13 11:11:56 +0200

    prettier config applied for js files

--- bot.js
+++ bot.js
@@ -1,18 +1,17 @@
 const config = require('./config.json');
-require('dotenv').config()
+require('dotenv').config();
 var token = config.token;
 
-console.log(process.env.NODE_ENV === 'production')
+console.log(process.env.NODE_ENV === 'production');
 
 var Bot = require('node-telegram-bot-api');
 var bot;
 
-if(process.env.NODE_ENV === 'production') {
-  bot = new Bot(token);
-  bot.setWebHook(config.url + bot.token);
-}
-else {
-  bot = new Bot(token, { polling: true });
+if (process.env.NODE_ENV === 'production') {
+	bot = new Bot(token);
+	bot.setWebHook(config.url + bot.token);
+} else {
+	bot = new Bot(token, { polling: true });
 }
 
 console.log('bot server started...');
@@ -24,121 +23,132 @@ const databaseName = 'vladislav';
 const uri = config.mongoUri;
 
 MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-    if (error) {
-        console.log(error)
-        return console.log('Unable to connect to database!');
-    }
+	if (error) {
+		console.log(error);
+		return console.log('Unable to connect to database!');
+	}
 
-    console.log('Connected correctly!');
+	console.log('Connected correctly!');
 
-    const db = client.db(databaseName);
+	const db = client.db(databaseName);
 
-    const kompromat = [
-      "https://i.pinimg.com/236x/ea/d1/6d/ead16d1a6aa69eeb1d935ce3adcd440b.jpg",
-      "https://boombo.biz/uploads/posts/2022-05/thumbs/1651626871_36-boombo-biz-p-yaoi-golie-parni-erotika-43.jpg",
-      "https://pw.artfile.me/wallpaper/11-03-2019/650x249/anime-kantai-collection-seksi-devushki-1444569.jpg",
-    ];
+	const kompromat = [
+		'https://i.pinimg.com/236x/ea/d1/6d/ead16d1a6aa69eeb1d935ce3adcd440b.jpg',
+		'https://boombo.biz/uploads/posts/2022-05/thumbs/1651626871_36-boombo-biz-p-yaoi-golie-parni-erotika-43.jpg',
+		'https://pw.artfile.me/wallpaper/11-03-2019/650x249/anime-kantai-collection-seksi-devushki-1444569.jpg'
+	];
 
-    const chatID = config.chatID;
+	const chatID = config.chatID;
 
-    let Answers = db.collection('answers');
-    let Randoms = db.collection('randoms');
+	let Answers = db.collection('answers');
+	let Randoms = db.collection('randoms');
 
-    bot.on('message', msg => {
-      const { text, chat: { id } } = msg;
-      if (text && !text.match(/^\//gm)) {
-          if (text.toLocaleLowerCase().trim().indexOf("найди компромат на") > -1) {
-              if (msg.entities && msg.entities[0]) {
-                  const mention = text.substr(msg.entities[0].offset + 1, msg.entities[0].length);
-                  if (mention.indexOf("golveronika") > -1) {
-                      bot.sendMessage(id, `На Веронику ничего не нашел`); 
-                  } else {
-                      const index = Math.floor(Math.random() * (kompromat.length));
-                      bot.sendPhoto(id, kompromat[index], {caption: `Смотри что я нашел в папке "C:\\\\Users\\${mention}\\drochka\\" у @${mention}`});
-                  }
-              }
-          } else {
-              Answers.findOne({ key: text.toLocaleLowerCase() }, (error, result) => {
-                  if (!error && result) {
-                      bot.sendMessage(id, `${result.answer}`); 
-                  }
-              });                
-          }
-      }
-    });
-   
-    bot.onText(/\/addAnswer (.+)/, (msg, match) => {
+	bot.on('message', (msg) => {
+		const {
+			text,
+			chat: { id }
+		} = msg;
+		if (text && !text.match(/^\//gm)) {
+			if (text.toLocaleLowerCase().trim().indexOf('найди компромат на') > -1) {
+				if (msg.entities && msg.entities[0]) {
+					const mention = text.substr(msg.entities[0].offset + 1, msg.entities[0].length);
+					if (mention.indexOf('golveronika') > -1) {
+						bot.sendMessage(id, `На Веронику ничего не нашел`);
+					} else {
+						const index = Math.floor(Math.random() * kompromat.length);
+						bot.sendPhoto(id, kompromat[index], {
+							caption: `Смотри что я нашел в папке "C:\\\\Users\\${mention}\\drochka\\" у @${mention}`
+						});
+					}
+				}
+			} else {
+				Answers.findOne({ key: text.toLocaleLowerCase() }, (error, result) => {
+					if (!error && result) {
+						bot.sendMessage(id, `${result.answer}`);
+					}
+				});
+			}
+		}
+	});
 
-      if (match[1]) {
-          const key = match[1].match(/^(.+)=/gm)[0].replace("=","").toLocaleLowerCase().trim();
-          const answer = match[1].replace(key,"").replace("=","");
+	bot.onText(/\/addAnswer (.+)/, (msg, match) => {
+		if (match[1]) {
+			const key = match[1]
+				.match(/^(.+)=/gm)[0]
+				.replace('=', '')
+				.toLocaleLowerCase()
+				.trim();
+			const answer = match[1].replace(key, '').replace('=', '');
 
-          if (key && answer) {
-              Answers.insertOne({
-                  key,
-                  answer
-              }, (error, result) => {
-                  if (!error) {
-                      bot.sendMessage(msg.chat.id, "Добавил."); 
-                  }
-              })                    
-          }
+			if (key && answer) {
+				Answers.insertOne(
+					{
+						key,
+						answer
+					},
+					(error, result) => {
+						if (!error) {
+							bot.sendMessage(msg.chat.id, 'Добавил.');
+						}
+					}
+				);
+			}
+		}
+	});
 
-      }
+	bot.onText(/\/addRandom (.+)/, (msg, match) => {
+		const text = match[1];
+		if (text) {
+			Randoms.insertOne(
+				{
+					text
+				},
+				(error, result) => {
+					if (error) {
+						return console.log('Unable to insert Random!');
+					} else {
+						bot.sendMessage(msg.chat.id, 'Спасибо.');
+					}
+				}
+			);
+		}
+	});
 
+	const schedule = require('node-schedule');
 
-    });
+	const sentRandom = async () => {
+		const length = await Randoms.count();
+		const index = Math.floor(Math.random() * length);
+		const message = await Randoms.findOne({}, { skip: index });
+		if (message) {
+			bot.sendMessage(chatID, message.text);
+		}
+	};
 
-    bot.onText(/\/addRandom (.+)/, (msg, match) => {
-      const text = match[1]; 
-      if (text) {
-          Randoms.insertOne({
-              text
-           }, (error, result) => {
-               if (error) {
-                   return console.log('Unable to insert Random!');
-               } else {
-                  bot.sendMessage(msg.chat.id, "Спасибо."); 
-               }
-           })           
-      }
-    });
+	const job = schedule.scheduleJob({ minute: 0 }, function () {
+		sentRandom();
+	});
+	const job2 = schedule.scheduleJob({ minute: 30 }, function () {
+		sentRandom();
+	});
 
-    const schedule = require('node-schedule');
+	// // hello command
+	// bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
+	//   var name = match[1];
+	//   bot.sendMessage(msg.chat.id, 'Hello s ' + name + '!').then(function () {
+	//     // reply sent!
+	//   });
+	// });
 
-    const sentRandom = async () => {
-      const length = await Randoms.count();
-      const index = Math.floor(Math.random() * (length));
-      const message = await Randoms.findOne({},{"skip": index,});
-      if (message) {
-          bot.sendMessage(chatID, message.text);
-      }
-    }
-
-    const job = schedule.scheduleJob({ minute: 0 }, function() {
-        sentRandom();
-    });
-    const job2 = schedule.scheduleJob({ minute: 30 }, function() {
-        sentRandom();
-    });
-
-// // hello command
-// bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
-//   var name = match[1];
-//   bot.sendMessage(msg.chat.id, 'Hello s ' + name + '!').then(function () {
-//     // reply sent!
-//   });
-// });
-
-// // sum command
-// bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
-//   var result = 0;
-//   match[1].trim().split(/\s+/).forEach(function (i) {
-//     result += (+i || 0);
-//   })
-//   bot.sendMessage(msg.chat.id, result).then(function () {
-//     // reply sent!
-//   });
+	// // sum command
+	// bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
+	//   var result = 0;
+	//   match[1].trim().split(/\s+/).forEach(function (i) {
+	//     result += (+i || 0);
+	//   })
+	//   bot.sendMessage(msg.chat.id, result).then(function () {
+	//     // reply sent!
+	//   });
 });
 
 module.exports = bot;
--- web.js
+++ web.js
@@ -1,27 +1,27 @@
 var express = require('express');
 var packageInfo = require('./package.json');
 var bodyParser = require('body-parser');
-require('dotenv').config()
+require('dotenv').config();
 
-console.log(process.env)
+console.log(process.env);
 
 var app = express();
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-  res.json({ version: packageInfo.version });
+	res.json({ version: packageInfo.version });
 });
 
 var server = app.listen(process.env.PORT, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+	var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('Web server started at http://%s:%s', host, port);
+	console.log('Web server started at http://%s:%s', host, port);
 });
 
 module.exports = function (bot) {
-  app.post('/' + bot.token, function (req, res) {
-    bot.processUpdate(req.body);
-    res.sendStatus(200);
-  });
+	app.post('/' + bot.token, function (req, res) {
+		bot.processUpdate(req.body);
+		res.sendStatus(200);
+	});
 };
